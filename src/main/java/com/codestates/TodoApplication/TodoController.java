b9fd2ae4344868749c4665bcb453aa7571f9c1dc
hyunwoojo <hwchokim95@hanmail.net>
2023-04-13 01:15:10 +0900

    hellocontroller출력수정

--- src/main/java/com/codestates/TodoApplication/TodoController.java
+++ src/main/java/com/codestates/TodoApplication/TodoController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TodoController {
     @GetMapping
     public String hello() {
-        return "hello everyone!!";
+        return "goodbye everyone!!";
     }
 }
